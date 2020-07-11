179369a6122c36286157aceb16f05c66afd9d34e
Aidan Timson <contact@timmo.xyz>
2020-07-11 11:29:13 +0100

    Add backend config to core

--- src/Types/Config.ts
+++ src/Types/Config.ts
@@ -1,5 +1,12 @@
+export interface ConfigBackend {
+  api_port: number;
+  secret: string;
+  token_expiry: string;
+}
+
 export default interface Config {
   log_level: string;
   services_directory: string;
   socket_port: number;
+  backend: ConfigBackend;
 }

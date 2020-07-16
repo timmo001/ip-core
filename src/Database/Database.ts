6f4113e54c400b5a7f14af22557146917fcf882f
Aidan Timson <contact@timmo.xyz>
2020-07-16 23:15:07 +0100

    Cleanup

--- src/Database/Database.ts
+++ src/Database/Database.ts
@@ -25,15 +25,15 @@ export default class Database extends Base {
     const connection: Connection = await this.pool.getConnection();
     await connection.query(
       'CREATE TABLE IF NOT EXISTS events (' +
-        'id VARCHAR(36) NOT NULL UNIQUE PRIMARY KEY,' +
-        'service VARCHAR(36) NOT NULL,' +
-        'status VARCHAR(52) NOT NULL,' +
-        'started DATETIME NOT NULL,' +
-        'updated DATETIME NOT NULL,' +
-        'completed DATETIME,' +
-        'message VARCHAR(255))' +
+        'id VARCHAR(36) NOT NULL UNIQUE PRIMARY KEY, ' +
+        'service VARCHAR(36) NOT NULL, ' +
+        'status VARCHAR(52) NOT NULL, ' +
+        'started DATETIME NOT NULL, ' +
+        'updated DATETIME NOT NULL, ' +
+        'completed DATETIME, ' +
+        'message VARCHAR(255)) ' +
         'WITH SYSTEM VERSIONING PARTITION BY SYSTEM_TIME (' +
-        'PARTITION events_history HISTORY,' +
+        'PARTITION events_history HISTORY, ' +
         'PARTITION events_current CURRENT' +
         ');'
     );
